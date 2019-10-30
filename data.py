2269a16c3acfe5ae8b7f5d20f02c77bef9766c13
Robin970822 <717740827@qq.com>
2019-10-30 18:10:17 +0800

    person matrix, triu matrix and label

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import os
 import h5py
 import config
+import scipy.io
 
 import numpy as np
 
@@ -8,6 +9,7 @@ root = config.scz_Data
 data_path = config.data_path
 
 
+# 计算person相关矩阵
 def getPerson(filename='aal2_266.mat'):
     myfile = h5py.File(os.path.join(root, filename))
     data = [myfile[element[0]][:]
@@ -16,8 +18,49 @@ def getPerson(filename='aal2_266.mat'):
     return person
 
 
+# 获得标签
+def getLabel(filename='all_cov.mat'):
+    all_cov = scipy.io.loadmat(os.path.join(root, filename))
+    all_cov = all_cov['all_cov']
+    labels = all_cov[:, 0]
+    return labels
+
+
+# 获得头动数据
+def getFD(filename='meanFD_info.mat'):
+    meanFD = h5py.File(os.path.join(root, filename))
+    meanFD = meanFD['meanFD'][0]
+    return meanFD
+
+
+# 排除头动数据
+def select(labels, meanFD):
+    index = [(label == 0 and fd < 0.2) or (label == 1 and fd < 0.3)
+             for label, fd in zip(labels, meanFD)]
+    return index
+
+
+# 展开person矩阵上三角
+def triu(person):
+    temp = [person[i][i + 1: len(person)] for i in range(len(person))]
+    return np.concatenate(temp)
+
+
+def saveData(data, root=data_path, filename='default.npy'):
+    path = os.path.join(root, filename)
+    np.save(path, data)
+
+
 if __name__ == '__main__':
     person = getPerson()
-    person_path = os.path.join(data_path, 'person.npy')
-    print(person_path)
-    np.save(person_path, person)
+    labels = getLabel()
+    fds = getFD()
+
+    index = select(labels, fds)
+    person = person[index]
+    labels = labels[index]
+    trius = [triu(a_person) for a_person in person]
+
+    saveData(person, filename='person.npy')
+    saveData(labels, filename='labels.npy')
+    saveData(trius, filename='trius.npy')
